e8de53c4bc4f35efe62ced343e92fcd4b1000990
bharath <broxdeez@gmail.com>
2022-07-21 21:57:15 -0700

    init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='test docker functionality for ML',
+    author='Bharath Mohan',
+    license='MIT',
+)
